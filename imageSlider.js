ea6bebf88161372cc66e1f90972682ec6856220b
Shivam kumar <ydvshivam10@gmail.com>
2024-05-13 13:37:46 +0530

    adding imageslider function.

--- imageSlider.js
+++ imageSlider.js
@@ -1,26 +1,23 @@
 const slides = document.querySelectorAll(".slide");
-var counter = 0;
-slides.forEach(
-    (slide,index) => {
-        slide.style.left = `${index * 100}%`;
-    }
-)
 
-const goNext = () => {
+slides.forEach((slide,index)=>{
+    slide.style.left = `${index*100}%`;
+})
+var counter = 0;
+const goNext = () =>
+{
     counter++;
     slideImage();
 }
 
-const goPrev = () => {
+const goPrev = ()=>{
     counter--;
     slideImage();
 }
 
 const slideImage = () =>{
-    slides.forEach(
-        (slide) =>
-        {
-            slide.style.transform = `translateX(-${counter*100}%)`
-        }
-    )
+    slides.forEach((slide)=>{
+        slide.style.transform = `translateX(-${counter*100}%)`;
+    })
 }
+
